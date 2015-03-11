05d7fcbdb624d0383913c7d37899e695a2598fd1
learrsan <gaslas@hotmail.com>
2015-03-11 16:57:11 +0100

    T11/3/15

---
+++ EjercicioCoches/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EjercicioCoches.Models;
+
+namespace EjercicioCoches.Controllers
+{
+    public class HomeController : Controller
+    {
+        private VehiculosEntities db = new VehiculosEntities();
+
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            var db = new VehiculosEntities();
+            return View(db.Vehiculo.ToList());
+
+        }
+
+        [HttpPost]
+        public ActionResult Alta(Vehiculo model)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Vehiculo.Add(model);
+                foreach (var idvehiculo in model.IdVehiculo)
+                {
+                    var c = db.Conductor.Find(idvehiculo);
+                    model.Conductor.Add(c);
+                }
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+        
+        }
+
+    }
+     
+
